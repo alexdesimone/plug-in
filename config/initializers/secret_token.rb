cde8cef35aadb2bdf613981276b29411bacea4c3
Alex De Simone <ajdesimone@gmail.com>
2012-07-01 10:25:51 -0700

    initiaize databasedotcom test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SfLeadGen::Application.config.secret_token = '087a929f9aaa476ffdf336824d70de6aa2c66da6865079564f42bd158de9fbfd09d80e6d64ba4668997bbbdf3cd14ef47d2a65ad5fe699f1ee969d892339c173'
